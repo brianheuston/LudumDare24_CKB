f6f54b65b49034e21ce581791c882a86179e21c7
Erik Schaeffer <gold_rabite@yahoo.com>
2012-08-25 12:52:23 -0700

    item icons
    generated icons now have a .icon which has image (string for image
    filename) and location ((x,y) for icon position in image)

--- HelloWorld/Items.js
+++ HelloWorld/Items.js
@@ -26,7 +26,9 @@ var items = function items() {
       basePowerTypes: [bonuses.damage, bonuses.attackSpeed, bonuses.defense],
       basePowerWeights: [4, 3, 3],
       extraPowerTypes: [bonuses.moveSpeed, bonuses.health, bonuses.mana, bonuses.regen, bonuses.focus, bonuses.spirit],
-      extraPowerWeights: [2, 1, 1, 2, 1, 1]
+      extraPowerWeights: [2, 1, 1, 2, 1, 1],
+      imageLocation: "lofi_obj.png",
+      iconLocations: [[5,3], [6,3], [7,3], [8,3]]
     },
     bow: {
       name: "bow",
@@ -35,6 +37,8 @@ var items = function items() {
       basePowerWeights: [3, 3],
       extraPowerTypes: [bonuses.moveSpeed, bonuses.health, bonuses.mana, bonuses.regen, bonuses.focus, bonuses.defense, bonuses.spirit],
       extraPowerWeights: [3, 1, 1, 1, 2, 1, 1]
+      imageLocation: "lofi_obj.png",
+      iconLocations: [[0,4],[1,4],[2,4],[3,4]]
     },
     staff: {
       name: "staff",
@@ -42,7 +46,9 @@ var items = function items() {
       basePowerTypes: [bonuses.damage, bonuses.attackSpeed, bonuses.focus],
       basePowerWeights: [3, 2, 3],
       extraPowerTypes: [bonuses.moveSpeed, bonuses.health, bonuses.mana, bonuses.regen, bonuses.defense, bonuses.spirit],
-      extraPowerWeights: [1, 1, 3, 1, 1, 3]
+      extraPowerWeights: [1, 1, 3, 1, 1, 3],
+      imageLocation: "lofi_obj.png",
+      iconLocations: [[12,3],[13,3],[14,3],[15,3]]
     },
     armor: {
       name: "armor",
@@ -50,7 +56,9 @@ var items = function items() {
       basePowerTypes: [bonuses.defense, bonuses.spirit],
       basePowerWeights: [3, 2],
       extraPowerTypes: [bonuses.damage, bonuses.attackSpeed, bonuses.moveSpeed, bonuses.health, bonuses.mana, bonuses.regen, bonuses.focus],
-      extraPowerWeights: [1, 1, 1, 3, 1, 2, 1]
+      extraPowerWeights: [1, 1, 1, 3, 1, 2, 1],
+      imageLocation: "lofi_obj.png",
+      iconLocations: [[4,4],[5,4],[6,4],[7,4],[8,4],[9,4],[10,4]]
     },
     robe: {
       name: "robe",
@@ -58,7 +66,9 @@ var items = function items() {
       basePowerTypes: [bonuses.defense, bonuses.spirit],
       basePowerWeights: [2, 3],
       extraPowerTypes: [bonuses.damage, bonuses.attackSpeed, bonuses.moveSpeed, bonuses.health, bonuses.mana, bonuses.regen, bonuses.focus],
-      extraPowerWeights: [1, 1, 1, 2, 2, 2, 2]
+      extraPowerWeights: [1, 1, 1, 2, 2, 2, 2],
+      imageLocation: "lofi_obj_packA.png",
+      iconLocations: [[0,8],[1,8],[2,8],[3,8],[4,8]]
     },
     shield: {
       name: "shield",
@@ -66,7 +76,9 @@ var items = function items() {
       basePowerTypes: [bonuses.defense],
       basePowerWeights: [4],
       extraPowerTypes: [bonuses.damage, bonuses.attackSpeed, bonuses.moveSpeed, bonuses.health, bonuses.mana, bonuses.regen, bonuses.focus, bonuses.spirit],
-      extraPowerWeights: [2, 1, 1, 2, 1, 2, 1, 1]
+      extraPowerWeights: [2, 1, 1, 2, 1, 2, 1, 1],
+      imageLocation: "lofi_obj.png",
+      iconLocations: [[0,3],[1,3],[2,3],[3,3],[4,3]]
     },
     idol: {
       name: "idol",
@@ -74,7 +86,9 @@ var items = function items() {
       basePowerTypes: [bonuses.mana],
       basePowerWeights: [4],
       extraPowerTypes: [bonuses.damage, bonuses.attackSpeed, bonuses.moveSpeed, bonuses.health, bonuses.regen, bonuses.focus, bonuses.defense, bonuses.spirit],
-      extraPowerWeights: [2, 1, 1, 1, 2, 1, 1, 2]
+      extraPowerWeights: [2, 1, 1, 1, 2, 1, 1, 2],
+      imageLocation: "lofi_obj.png",
+      iconLocations: [[9,3], [10,3]]
     },
     book: {
       name: "book",
@@ -82,7 +96,9 @@ var items = function items() {
       basePowerTypes: [bonuses.focus],
       basePowerWeights: [3],
       extraPowerTypes: [bonuses.damage, bonuses.attackSpeed, bonuses.moveSpeed, bonuses.health, bonuses.mana, bonuses.regen, bonuses.defense, bonuses.spirit],
-      extraPowerWeights: [2, 1, 1, 1, 2, 1, 1, 2]
+      extraPowerWeights: [2, 1, 1, 1, 2, 1, 1, 2],
+      imageLocation: "lofi_obj_packA.png",
+      iconLocations: [[0,2],[1,2],[2,2],[3,2],[4,2],[5,2],[6,2]]
     },
     quiver: {
       name: "quiver",
@@ -90,7 +106,9 @@ var items = function items() {
       basePowerTypes: [bonuses.attackSpeed],
       basePowerWeights: [4],
       extraPowerTypes: [bonuses.damage, bonuses.moveSpeed, bonuses.health, bonuses.mana, bonuses.regen, bonuses.focus, bonuses.defense, bonuses.spirit],
-      extraPowerWeights: [2, 3, 1, 1, 1, 1, 1, 1]
+      extraPowerWeights: [2, 3, 1, 1, 1, 1, 1, 1],
+      imageLocation: "lofi_obj_packA.png",
+      iconLocations: [[0,4],[1,4],[2,4],[3,4],[4,4]]
     },
     gloves: {
       name: "gloves",
@@ -98,7 +116,9 @@ var items = function items() {
       basePowerTypes: [bonuses.damage],
       basePowerWeights: [3],
       extraPowerTypes: [bonuses.attackSpeed, bonuses.moveSpeed, bonuses.health, bonuses.mana, bonuses.regen, bonuses.focus, bonuses.defense, bonuses.spirit],
-      extraPowerWeights: [3, 1, 1, 1, 1, 1, 1, 1]
+      extraPowerWeights: [3, 1, 1, 1, 1, 1, 1, 1],
+      imageLocation: "lofi_obj_packA.png",
+      iconLocations: [[0,5],[1,5],[2,5],[3,5],[4,5]]
     },
     ring: {
       name: "ring",
@@ -106,7 +126,9 @@ var items = function items() {
       basePowerTypes: [bonuses.focus],
       basePowerWeights: [3],
       extraPowerTypes: [bonuses.damage, bonuses.attackSpeed, bonuses.moveSpeed, bonuses.health, bonuses.mana, bonuses.regen, bonuses.defense, bonuses.spirit],
-      extraPowerWeights: [1, 1, 1, 2, 2, 2, 2, 2]
+      extraPowerWeights: [1, 1, 1, 2, 2, 2, 2, 2],
+      imageLocation: "lofi_obj.png",
+      iconLocations: [[6,2],[7,2],[8,2],[9,2],[10,2]]
     },
     helmet: {
       name: "helmet",
@@ -114,7 +136,9 @@ var items = function items() {
       basePowerTypes: [bonuses.defense],
       basePowerWeights: [3],
       extraPowerTypes: [bonuses.damage, bonuses.attackSpeed, bonuses.moveSpeed, bonuses.health, bonuses.mana, bonuses.regen, bonuses.focus, bonuses.spirit],
-      extraPowerWeights: [1, 1, 1, 2, 1, 2, 1, 1]
+      extraPowerWeights: [1, 1, 1, 2, 1, 2, 1, 1],
+      imageLocation: "lofi_obj_packA.png",
+      iconLocations: [[0,4],[1,4],[2,4],[3,4],[4,4],[5,4],[6,4]]
     },
     hood: {
       name: "hood",
@@ -122,7 +146,9 @@ var items = function items() {
       basePowerTypes: [bonuses.spirit],
       basePowerWeights: [3],
       extraPowerTypes: [bonuses.damage, bonuses.attackSpeed, bonuses.moveSpeed, bonuses.health, bonuses.mana, bonuses.regen, bonuses.focus, bonuses.defense],
-      extraPowerWeights: [1, 1, 1, 2, 2, 2, 2, 1]
+      extraPowerWeights: [1, 1, 1, 2, 2, 2, 2, 1],
+      imageLocation: "lofi_obj_packA.png",
+      iconLocations: [[0,6],[1,6],[2,6],[3,6],[4,6]]
     }
   };
   var names = ["sword", "bow", "staff", "armor", "robe", "shield", "idol", "book", "quiver", "gloves", "ring", "helmet", "hood"];
@@ -156,8 +182,13 @@ var items = function items() {
     return {powerNames: powerNames, powerLevels: powerLevels};
   };
   
-  var createRandom = function createRandom(power) {
-    var itemType = types[names[choose(probabilities)]];
+  var createRandom = function createRandom(power, type) {
+    var itemType
+    if (!type) {
+      itemType = types[names[choose(probabilities)]];
+    } else {
+      itemType = types[type];
+    }
     var basePowers = createBasePowers(itemType);
     var powerNames = basePowers.powerNames;
     var powerLevels = basePowers.powerLevels;
@@ -183,10 +214,12 @@ var items = function items() {
       powerLevels[powerPlus] = powerLevels[powerPlus] + 1;
     }
     
-    return {name: itemType.name, type: itemType.name, power: power, powerNames: powerNames, powerLevels: powerLevels};
+    var icon = {image: itemType.imageLocation, location: itemType.iconLocations[Math.floor(Math.random() * itemType.iconLocations.length)]};
+    
+    return {name: itemType.name, type: itemType.name, power: power, powerNames: powerNames, powerLevels: powerLevels, icon:icon};
   };
   
-  var mix = function mixItems(item1, item2) {
+  var mixItems = function mixItems(item1, item2) {
     var itemType, newType;
     
     if (item1.type === item2.type) {
@@ -239,8 +272,10 @@ var items = function items() {
       var powerPlus = choose(powerWeights);
       powerLevels[powerPlus] = powerLevels[powerPlus] + 1;
     }
+
+    var icon = {image: itemType.imageLocation, location: itemType.iconLocations[Math.floor(Math.random() * itemType.iconLocations.length)]};
     
-    return {name: itemType.name, type: itemType.name, power: power, powerNames: powerNames, powerLevels: powerLevels};
+    return {name: itemType.name, type: itemType.name, power: power, powerNames: powerNames, powerLevels: powerLevels, icon: icon};
   };
   
   return {
@@ -250,6 +285,6 @@ var items = function items() {
     names: names,
     probabilities: probabilities,
     createRandom: createRandom,
-    mix: mix
+    mixItems: mixItems
   }
 }();
