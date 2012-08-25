1164aed3b3ca30fc84d737454d4de2d5b28d648e
Erik Schaeffer <gold_rabite@yahoo.com>
2012-08-25 14:09:52 -0700

    Added item stats

--- HelloWorld/Items.js
+++ HelloWorld/Items.js
@@ -1,16 +1,4 @@
 var items = function items() {
-  var bonuses = {
-    damage:"damage",
-    attackSpeed:"attackSpeed",
-    moveSpeed:"moveSpeed",
-    health:"health",
-    mana:"mana",
-    regen:"regen",
-    focus:"focus",
-    defense:"defense",
-    spirit:"spirit",
-  };
-
   var slots = {
     weapon: "weapon",
     torso: "torso",
@@ -23,130 +11,130 @@ var items = function items() {
     sword: {
       name: "sword",
       slot: slots.weapon,
-      basePowerTypes: [bonuses.damage, bonuses.attackSpeed, bonuses.defense],
-      basePowerWeights: [4, 3, 3],
-      extraPowerTypes: [bonuses.moveSpeed, bonuses.health, bonuses.mana, bonuses.regen, bonuses.focus, bonuses.spirit],
-      extraPowerWeights: [2, 1, 1, 2, 1, 1],
+      baseStatTypes: [stats.names.damage, stats.names.attackSpeed, stats.names.defense],
+      baseStatWeights: [4, 3, 3],
+      extraStatTypes: [stats.names.moveSpeed, stats.names.health, stats.names.mana, stats.names.regen, stats.names.focus, stats.names.spirit],
+      extraStatWeights: [2, 1, 1, 2, 1, 1],
       imageLocation: "lofi_obj.png",
       iconLocations: [[5,3], [6,3], [7,3], [8,3]]
     },
     bow: {
       name: "bow",
       slot: slots.weapon,
-      basePowerTypes: [bonuses.damage, bonuses.attackSpeed],
-      basePowerWeights: [3, 3],
-      extraPowerTypes: [bonuses.moveSpeed, bonuses.health, bonuses.mana, bonuses.regen, bonuses.focus, bonuses.defense, bonuses.spirit],
-      extraPowerWeights: [3, 1, 1, 1, 2, 1, 1]
+      baseStatTypes: [stats.names.damage, stats.names.attackSpeed],
+      baseStatWeights: [3, 3],
+      extraStatTypes: [stats.names.moveSpeed, stats.names.health, stats.names.mana, stats.names.regen, stats.names.focus, stats.names.defense, stats.names.spirit],
+      extraStatWeights: [3, 1, 1, 1, 2, 1, 1],
       imageLocation: "lofi_obj.png",
       iconLocations: [[0,4],[1,4],[2,4],[3,4]]
     },
     staff: {
       name: "staff",
       slot: slots.weapon,
-      basePowerTypes: [bonuses.damage, bonuses.attackSpeed, bonuses.focus],
-      basePowerWeights: [3, 2, 3],
-      extraPowerTypes: [bonuses.moveSpeed, bonuses.health, bonuses.mana, bonuses.regen, bonuses.defense, bonuses.spirit],
-      extraPowerWeights: [1, 1, 3, 1, 1, 3],
+      baseStatTypes: [stats.names.damage, stats.names.attackSpeed, stats.names.focus],
+      baseStatWeights: [3, 2, 3],
+      extraStatTypes: [stats.names.moveSpeed, stats.names.health, stats.names.mana, stats.names.regen, stats.names.defense, stats.names.spirit],
+      extraStatWeights: [1, 1, 3, 1, 1, 3],
       imageLocation: "lofi_obj.png",
       iconLocations: [[12,3],[13,3],[14,3],[15,3]]
     },
     armor: {
       name: "armor",
       slot: slots.torso,
-      basePowerTypes: [bonuses.defense, bonuses.spirit],
-      basePowerWeights: [3, 2],
-      extraPowerTypes: [bonuses.damage, bonuses.attackSpeed, bonuses.moveSpeed, bonuses.health, bonuses.mana, bonuses.regen, bonuses.focus],
-      extraPowerWeights: [1, 1, 1, 3, 1, 2, 1],
+      baseStatTypes: [stats.names.defense, stats.names.spirit],
+      baseStatWeights: [3, 2],
+      extraStatTypes: [stats.names.damage, stats.names.attackSpeed, stats.names.moveSpeed, stats.names.health, stats.names.mana, stats.names.regen, stats.names.focus],
+      extraStatWeights: [1, 1, 1, 3, 1, 2, 1],
       imageLocation: "lofi_obj.png",
       iconLocations: [[4,4],[5,4],[6,4],[7,4],[8,4],[9,4],[10,4]]
     },
     robe: {
       name: "robe",
       slot: slots.torso,
-      basePowerTypes: [bonuses.defense, bonuses.spirit],
-      basePowerWeights: [2, 3],
-      extraPowerTypes: [bonuses.damage, bonuses.attackSpeed, bonuses.moveSpeed, bonuses.health, bonuses.mana, bonuses.regen, bonuses.focus],
-      extraPowerWeights: [1, 1, 1, 2, 2, 2, 2],
+      baseStatTypes: [stats.names.defense, stats.names.spirit],
+      baseStatWeights: [2, 3],
+      extraStatTypes: [stats.names.damage, stats.names.attackSpeed, stats.names.moveSpeed, stats.names.health, stats.names.mana, stats.names.regen, stats.names.focus],
+      extraStatWeights: [1, 1, 1, 2, 2, 2, 2],
       imageLocation: "lofi_obj_packA.png",
       iconLocations: [[0,8],[1,8],[2,8],[3,8],[4,8]]
     },
     shield: {
       name: "shield",
       slot: slots.offhand,
-      basePowerTypes: [bonuses.defense],
-      basePowerWeights: [4],
-      extraPowerTypes: [bonuses.damage, bonuses.attackSpeed, bonuses.moveSpeed, bonuses.health, bonuses.mana, bonuses.regen, bonuses.focus, bonuses.spirit],
-      extraPowerWeights: [2, 1, 1, 2, 1, 2, 1, 1],
+      baseStatTypes: [stats.names.defense],
+      baseStatWeights: [4],
+      extraStatTypes: [stats.names.damage, stats.names.attackSpeed, stats.names.moveSpeed, stats.names.health, stats.names.mana, stats.names.regen, stats.names.focus, stats.names.spirit],
+      extraStatWeights: [2, 1, 1, 2, 1, 2, 1, 1],
       imageLocation: "lofi_obj.png",
       iconLocations: [[0,3],[1,3],[2,3],[3,3],[4,3]]
     },
     idol: {
       name: "idol",
       slot: slots.offhand,
-      basePowerTypes: [bonuses.mana],
-      basePowerWeights: [4],
-      extraPowerTypes: [bonuses.damage, bonuses.attackSpeed, bonuses.moveSpeed, bonuses.health, bonuses.regen, bonuses.focus, bonuses.defense, bonuses.spirit],
-      extraPowerWeights: [2, 1, 1, 1, 2, 1, 1, 2],
+      baseStatTypes: [stats.names.mana],
+      baseStatWeights: [4],
+      extraStatTypes: [stats.names.damage, stats.names.attackSpeed, stats.names.moveSpeed, stats.names.health, stats.names.regen, stats.names.focus, stats.names.defense, stats.names.spirit],
+      extraStatWeights: [2, 1, 1, 1, 2, 1, 1, 2],
       imageLocation: "lofi_obj.png",
       iconLocations: [[9,3], [10,3]]
     },
     book: {
       name: "book",
       slot: slots.offhand,
-      basePowerTypes: [bonuses.focus],
-      basePowerWeights: [3],
-      extraPowerTypes: [bonuses.damage, bonuses.attackSpeed, bonuses.moveSpeed, bonuses.health, bonuses.mana, bonuses.regen, bonuses.defense, bonuses.spirit],
-      extraPowerWeights: [2, 1, 1, 1, 2, 1, 1, 2],
+      baseStatTypes: [stats.names.focus],
+      baseStatWeights: [3],
+      extraStatTypes: [stats.names.damage, stats.names.attackSpeed, stats.names.moveSpeed, stats.names.health, stats.names.mana, stats.names.regen, stats.names.defense, stats.names.spirit],
+      extraStatWeights: [2, 1, 1, 1, 2, 1, 1, 2],
       imageLocation: "lofi_obj_packA.png",
       iconLocations: [[0,2],[1,2],[2,2],[3,2],[4,2],[5,2],[6,2]]
     },
     quiver: {
       name: "quiver",
       slot: slots.offhand,
-      basePowerTypes: [bonuses.attackSpeed],
-      basePowerWeights: [4],
-      extraPowerTypes: [bonuses.damage, bonuses.moveSpeed, bonuses.health, bonuses.mana, bonuses.regen, bonuses.focus, bonuses.defense, bonuses.spirit],
-      extraPowerWeights: [2, 3, 1, 1, 1, 1, 1, 1],
+      baseStatTypes: [stats.names.attackSpeed],
+      baseStatWeights: [4],
+      extraStatTypes: [stats.names.damage, stats.names.moveSpeed, stats.names.health, stats.names.mana, stats.names.regen, stats.names.focus, stats.names.defense, stats.names.spirit],
+      extraStatWeights: [2, 3, 1, 1, 1, 1, 1, 1],
       imageLocation: "lofi_obj_packA.png",
       iconLocations: [[0,4],[1,4],[2,4],[3,4],[4,4]]
     },
     gloves: {
       name: "gloves",
       slot: slots.hands,
-      basePowerTypes: [bonuses.damage],
-      basePowerWeights: [3],
-      extraPowerTypes: [bonuses.attackSpeed, bonuses.moveSpeed, bonuses.health, bonuses.mana, bonuses.regen, bonuses.focus, bonuses.defense, bonuses.spirit],
-      extraPowerWeights: [3, 1, 1, 1, 1, 1, 1, 1],
+      baseStatTypes: [stats.names.damage],
+      baseStatWeights: [3],
+      extraStatTypes: [stats.names.attackSpeed, stats.names.moveSpeed, stats.names.health, stats.names.mana, stats.names.regen, stats.names.focus, stats.names.defense, stats.names.spirit],
+      extraStatWeights: [3, 1, 1, 1, 1, 1, 1, 1],
       imageLocation: "lofi_obj_packA.png",
       iconLocations: [[0,5],[1,5],[2,5],[3,5],[4,5]]
     },
     ring: {
       name: "ring",
       slot: slots.hands,
-      basePowerTypes: [bonuses.focus],
-      basePowerWeights: [3],
-      extraPowerTypes: [bonuses.damage, bonuses.attackSpeed, bonuses.moveSpeed, bonuses.health, bonuses.mana, bonuses.regen, bonuses.defense, bonuses.spirit],
-      extraPowerWeights: [1, 1, 1, 2, 2, 2, 2, 2],
+      baseStatTypes: [stats.names.focus],
+      baseStatWeights: [3],
+      extraStatTypes: [stats.names.damage, stats.names.attackSpeed, stats.names.moveSpeed, stats.names.health, stats.names.mana, stats.names.regen, stats.names.defense, stats.names.spirit],
+      extraStatWeights: [1, 1, 1, 2, 2, 2, 2, 2],
       imageLocation: "lofi_obj.png",
       iconLocations: [[6,2],[7,2],[8,2],[9,2],[10,2]]
     },
     helmet: {
       name: "helmet",
       slot: slots.head,
-      basePowerTypes: [bonuses.defense],
-      basePowerWeights: [3],
-      extraPowerTypes: [bonuses.damage, bonuses.attackSpeed, bonuses.moveSpeed, bonuses.health, bonuses.mana, bonuses.regen, bonuses.focus, bonuses.spirit],
-      extraPowerWeights: [1, 1, 1, 2, 1, 2, 1, 1],
+      baseStatTypes: [stats.names.defense],
+      baseStatWeights: [3],
+      extraStatTypes: [stats.names.damage, stats.names.attackSpeed, stats.names.moveSpeed, stats.names.health, stats.names.mana, stats.names.regen, stats.names.focus, stats.names.spirit],
+      extraStatWeights: [1, 1, 1, 2, 1, 2, 1, 1],
       imageLocation: "lofi_obj_packA.png",
       iconLocations: [[0,4],[1,4],[2,4],[3,4],[4,4],[5,4],[6,4]]
     },
     hood: {
       name: "hood",
       slot: slots.head,
-      basePowerTypes: [bonuses.spirit],
-      basePowerWeights: [3],
-      extraPowerTypes: [bonuses.damage, bonuses.attackSpeed, bonuses.moveSpeed, bonuses.health, bonuses.mana, bonuses.regen, bonuses.focus, bonuses.defense],
-      extraPowerWeights: [1, 1, 1, 2, 2, 2, 2, 1],
+      baseStatTypes: [stats.names.spirit],
+      baseStatWeights: [3],
+      extraStatTypes: [stats.names.damage, stats.names.attackSpeed, stats.names.moveSpeed, stats.names.health, stats.names.mana, stats.names.regen, stats.names.focus, stats.names.defense],
+      extraStatWeights: [1, 1, 1, 2, 2, 2, 2, 1],
       imageLocation: "lofi_obj_packA.png",
       iconLocations: [[0,6],[1,6],[2,6],[3,6],[4,6]]
     }
@@ -171,15 +159,15 @@ var items = function items() {
   };
   
   var createBasePowers = function createBasePowers(itemType) {
-    var powerNames = [];
-    var powerLevels = [];
+    var statNames = [];
+    var statLevels = [];
     
-    for (var i = 0; i < itemType.basePowerTypes.length; i++) {
-      powerNames.push(itemType.basePowerTypes[i]);
-      powerLevels.push(1);
+    for (var i = 0; i < itemType.baseStatTypes.length; i++) {
+      statNames.push(itemType.baseStatTypes[i]);
+      statLevels.push(1);
     }
     
-    return {powerNames: powerNames, powerLevels: powerLevels};
+    return {statNames: statNames, statLevels: statLevels};
   };
   
   var createRandom = function createRandom(power, type) {
@@ -190,36 +178,44 @@ var items = function items() {
       itemType = types[type];
     }
     var basePowers = createBasePowers(itemType);
-    var powerNames = basePowers.powerNames;
-    var powerLevels = basePowers.powerLevels;
+    var statNames = basePowers.statNames;
+    var statLevels = basePowers.statLevels;
     
-    var powerWeights = itemType.basePowerWeights.slice(0);
-    var newPowers = itemType.extraPowerTypes.slice(0);
-    var newPowersWeight = itemType.extraPowerWeights.slice(0);
+    var StatWeights = itemType.baseStatWeights.slice(0);
+    var newStats = itemType.extraStatTypes.slice(0);
+    var newStatsWeight = itemType.extraStatWeights.slice(0);
     
     for (var i = 0; i < power; i++) {
-      if (powerWeights.length <= 8) {
-        if (Math.random() < 1 / (powerWeights.length * powerWeights.length + 1)) {
-          var newPower = choose(newPowersWeight);
-          powerNames.push(newPowers[newPower]);
-          powerLevels.push(1);
-          powerWeights.push(newPowersWeight[newPower]);
-          newPowers.splice(newPower, 1);
-          newPowersWeight.splice(newPower, 1);
+      if (StatWeights.length <= 8) {
+        if (Math.random() < 1 / (StatWeights.length * StatWeights.length + 1)) {
+          var newStat = choose(newStatsWeight);
+          statNames.push(newStats[newStat]);
+          statLevels.push(1);
+          StatWeights.push(newStatsWeight[newStat]);
+          newStats.splice(newStat, 1);
+          newStatsWeight.splice(newStat, 1);
           continue;
         }
       }
       
-      var powerPlus = choose(powerWeights);
-      powerLevels[powerPlus] = powerLevels[powerPlus] + 1;
+      var powerPlus = choose(StatWeights);
+      statLevels[powerPlus] = statLevels[powerPlus] + 1;
     }
     
     var icon = {image: itemType.imageLocation, location: itemType.iconLocations[Math.floor(Math.random() * itemType.iconLocations.length)]};
     
-    return {name: itemType.name, type: itemType.name, power: power, powerNames: powerNames, powerLevels: powerLevels, icon:icon};
+    return {
+      name: itemType.name,
+      type: itemType.name,
+      power: power,
+      statNames: statNames,
+      statLevels: statLevels,
+      icon: icon,
+      slot: itemType.slot
+    };
   };
   
-  var mixItems = function mixItems(item1, item2) {
+  var mix = function mix(item1, item2) {
     var itemType, newType;
     
     if (item1.type === item2.type) {
@@ -234,57 +230,64 @@ var items = function items() {
                                         (Math.random() - 0.5)) / Math.sqrt(2));
     
     var basePowers = createBasePowers(itemType);
-    var powerNames = basePowers.powerNames;
-    var powerLevels = basePowers.powerLevels;
+    var statNames = basePowers.statNames;
+    var statLevels = basePowers.statLevels;
     
-    var powerWeights = []
-    var newPowers = [];
-    var newPowersWeight = [];
+    var StatWeights = []
+    var newStats = [];
+    var newStatsWeight = [];
     
     var items = [item1, item2];
     for (var j = 0; j < items.length; j++) {
       var currentItem = items[j];
     
-      for (var i = 0; i < currentItem.powerNames.length; i++) {
-        var indexOfPower = powerNames.indexOf(currentItem.powerNames[i]);
+      for (var i = 0; i < currentItem.statNames.length; i++) {
+        var indexOfPower = statNames.indexOf(currentItem.statNames[i]);
         if (indexOfPower === -1) {
-          newPowers.push(currentItem.powerNames[i]);
-          newPowersWeight.push(currentItem.powerLevels[i]);
+          newStats.push(currentItem.statNames[i]);
+          newStatsWeight.push(currentItem.statLevels[i]);
         } else {
-          powerWeights[indexOfPower] = (powerWeights[indexOfPower] || 0) + currentItem.powerLevels[i];
+          StatWeights[indexOfPower] = (StatWeights[indexOfPower] || 0) + currentItem.statLevels[i];
         }
       }
     }
     
     for (var i = 0; i < power; i++) {
-      if (powerWeights.length <= 8 && newPowers.length > 0) {
-        if (Math.random() < 1 / (powerWeights.length * powerWeights.length + 1)) {
-          var newPower = choose(newPowersWeight);
-          powerNames.push(newPowers[newPower]);
-          powerLevels.push(1);
-          powerWeights.push(newPowersWeight[newPower]);
-          newPowers.splice(newPower, 1);
-          newPowersWeight.splice(newPower, 1);
+      if (StatWeights.length <= 8 && newStats.length > 0) {
+        if (Math.random() < 1 / (StatWeights.length * StatWeights.length + 1)) {
+          var newStat = choose(newStatsWeight);
+          statNames.push(newStats[newStat]);
+          statLevels.push(1);
+          StatWeights.push(newStatsWeight[newStat]);
+          newStats.splice(newStat, 1);
+          newStatsWeight.splice(newStat, 1);
           continue;
         }
       }
       
-      var powerPlus = choose(powerWeights);
-      powerLevels[powerPlus] = powerLevels[powerPlus] + 1;
+      var powerPlus = choose(StatWeights);
+      statLevels[powerPlus] = statLevels[powerPlus] + 1;
     }
 
     var icon = {image: itemType.imageLocation, location: itemType.iconLocations[Math.floor(Math.random() * itemType.iconLocations.length)]};
     
-    return {name: itemType.name, type: itemType.name, power: power, powerNames: powerNames, powerLevels: powerLevels, icon: icon};
+    return {
+      name: itemType.name,
+      type: itemType.name,
+      power: power,
+      statNames: statNames,
+      statLevels: statLevels,
+      icon: icon,
+      slot: itemType.slot
+    };
   };
   
   return {
-    bonuses: bonuses,
     slots: slots,
     types: types,
     names: names,
     probabilities: probabilities,
     createRandom: createRandom,
-    mixItems: mixItems
+    mix: mix
   }
 }();
---
+++ HelloWorld/Stats.js
@@ -0,0 +1,15 @@
+var stats = function() {
+  var names = {
+    health:"health",
+    mana:"mana",
+    damage:"damage",
+    attackSpeed:"attackSpeed",
+    moveSpeed:"moveSpeed",
+    regen:"regen",
+    focus:"focus",
+    defense:"defense",
+    spirit:"spirit",
+  };
+  
+  return {names: names};
+}();
