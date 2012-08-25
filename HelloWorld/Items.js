173aa9d5e094544e374a8ed7d083e490e4e31e51
Erik Schaeffer <gold_rabite@yahoo.com>
2012-08-24 23:13:00 -0700

    Added items and base resources

---
+++ HelloWorld/Items.js
@@ -0,0 +1,13 @@
+var skillTypes = {
+  damage:"damage",
+  attackSpeed:"attackSpeed",
+  moveSpeed:"moveSpeed",
+  health:"health",
+  mana:"mana",
+  regen:"regen",
+  focus:"focus",
+  defense:"defense",
+  spirit:"spirit",
+};
+
+skillTypes.damage, skillTypes.attackSpeed, skillTypes.moveSpeed, skillTypes.health, skillTypes.mana, skillTypes.regen, skillTypes.focus, skillTypes.defense, skillTypes.spirit
